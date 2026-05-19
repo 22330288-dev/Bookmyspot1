0a01809de89a656076d1c54c37655758a3b4d95f
dima-web <22330288@students.liu.edu.lb>
2026-05-20 01:09:06 +0300

    Update Login.js

--- bookmyspot/src/pages/Login.js
+++ bookmyspot/src/pages/Login.js
@@ -47,7 +47,7 @@ export default function Login() {
 
     if (Object.keys(validationErrors).length === 0) {
       try {
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("${process.env.REACT_APP_API_URL}/api/auth/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
